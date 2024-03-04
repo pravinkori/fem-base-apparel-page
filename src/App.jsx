96ee2f4c55f17439701b6c7642d73975bb93464d
Pravin Kori <pravinkori@protonmail.ch>
2024-03-04 18:09:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import HomePage from "./components/Home/Home";
+import "./css/App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <HomePage />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,40 @@
+import logo from "/src/assets/images/logo.svg";
+import mobileImage from "/src/assets/images/hero-mobile.jpg";
+import arrowIcon from "/src/assets/images/icon-arrow.svg";
+import styles from "./Home.module.css";
+
+function HomePage() {
+  return (
+    <>
+      <nav className={styles.logo}>
+        <img src={logo} alt="logo" />
+      </nav>
+      <section>
+        <picture className="">
+          <img src={mobileImage} alt="picture of a women" />
+        </picture>
+        <div className={styles["page-content"]}>
+          <h1 className={styles["page-title"]}>
+            <span>we&apos;re</span> coming soon
+          </h1>
+
+          <p className={styles["page-description"]}>
+            Hello fellow shoppers! We&apos;re currently building our new fashion
+            store. Add your email below to stay up-to-date with announcements
+            and our launch deals
+          </p>
+
+          <form>
+            <label htmlFor="email-input"></label>
+            <input placeholder="Email Address" type="email" id="email-input" />
+            <button aria-label="Submit" className="submit-btn">
+              <img alt="" src={arrowIcon} />
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default HomePage;
