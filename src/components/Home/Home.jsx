214b40ed14effca71244159b367b74340c91c48c
Pravin Kori <pravinkori@protonmail.ch>
2024-03-05 17:48:57 +0530

    style: update styling of form to improve layout and design

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import logo from "/src/assets/images/logo.svg";
 import mobileImage from "/src/assets/images/hero-mobile.jpg";
+import desktopImage from "/src/assets/images/hero-desktop.jpg";
 import arrowIcon from "/src/assets/images/icon-arrow.svg";
+import errorIcon from "/src/assets/images/icon-error.svg";
 import styles from "./Home.module.css";
 
 function HomePage() {
@@ -10,12 +12,13 @@ function HomePage() {
         <img src={logo} alt="logo" />
       </nav>
       <section className={styles.container}>
-        <picture className="">
+        <picture className={styles["hero-img"]}>
+          <source srcSet={desktopImage} media="(min-width: 600px)" />
           <img src={mobileImage} alt="picture of a women" />
         </picture>
         <div className={styles["page-content"]}>
           <h1 className={styles["page-title"]}>
-            <span>we&apos;re</span> coming soon
+            we&apos;re<span> coming soon</span>
           </h1>
 
           <p className={styles["page-description"]}>
@@ -25,16 +28,24 @@ function HomePage() {
           </p>
 
           <form>
-            <label htmlFor="email-input"></label>
-            <input
-              placeholder="Email Address"
-              type="email"
-              id="email-input"
-              className={styles["form-input"]}
-            />
+            <div className={styles["form-input"]}>
+              {/* <label htmlFor="email-input"></label> */}
+              <input
+                placeholder="Email Address"
+                type="email"
+                id="email-input"
+                // className={styles["form-input"]}
+              />
+              <span className={styles["error-icon"]}>
+                <img src={errorIcon} alt="" aria-hidden="true" />
+              </span>
+            </div>
             <button aria-label="Submit" className={styles["submit-btn"]}>
               <img alt="submit" src={arrowIcon} />
             </button>
+            <span className={styles["error-alert"]}>
+              Please provide a valid email
+            </span>
           </form>
         </div>
       </section>
